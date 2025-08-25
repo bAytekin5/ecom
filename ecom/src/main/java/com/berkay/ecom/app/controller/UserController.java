0af663dd2ea61ff17be3ceff19740571d1bf6477
Berkay Aytekin <berkay.0567@gmail.com>
2025-08-26 01:50:50 +0300

    any

--- ecom/src/main/java/com/berkay/ecom/app/controller/UserController.java
+++ ecom/src/main/java/com/berkay/ecom/app/controller/UserController.java
@@ -6,6 +6,8 @@ import com.berkay.ecom.app.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,14 +21,32 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/users")
-    public List<User> getUsers() {
-        return userService.fetchAllUsers();
+    public ResponseEntity<List<User>> getAllUsers() {
+        return new ResponseEntity<>(userService.fetchAllUsers(), HttpStatus.OK);
+//        return ResponseEntity.ok(userService.fetchAllUsers());
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        User user = userService.fetchUser(id);
+        if (user == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(user);
     }
 
     @PostMapping("/users")
-    public List<User> createUser(@RequestBody User user) {
+    public ResponseEntity<List<User>> createUser(@RequestBody User user) {
         userService.addUser(user);
-        log.info(user.toString() + " created user" + user.toString());
-        return userService.fetchAllUsers();
+        return ResponseEntity.ok(userService.fetchAllUsers());
+    }
+
+    @PutMapping("/users/update/{id}")
+    public ResponseEntity<String> updateUser(@PathVariable Long id, @RequestBody User user) {
+        boolean success = userService.updateUser(id, user);
+        if (success) {
+            return new ResponseEntity<>("User updated successfully", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("User update failed", HttpStatus.BAD_REQUEST);
     }
 }
--- ecom/src/main/java/com/berkay/ecom/app/service/UserService.java
+++ ecom/src/main/java/com/berkay/ecom/app/service/UserService.java
@@ -1,6 +1,7 @@
 package com.berkay.ecom.app.service;
 
 import com.berkay.ecom.app.model.User;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -10,13 +11,31 @@ import java.util.List;
 public class UserService {
 
     private List<User> users = new ArrayList<User>();
+    private Long nextId = 1L;
 
     public List<User> fetchAllUsers() {
         return users;
     }
 
-    public User addUser(User user) {
+    public void addUser(User user) {
+        user.setId(nextId++);
         users.add(user);
-        return user;
     }
+
+    public User fetchUser(Long id) {
+        return users.stream().filter(u -> u.getId() == id).findFirst().orElse(null);
+    }
+
+    public boolean updateUser(Long id, User updatedUser) {
+
+        return users.stream().filter(u -> u.getId() == id)
+                .findFirst()
+                .map(existingUser -> {
+                    existingUser.setFirstName(updatedUser.getFirstName());
+                    existingUser.setLastName(updatedUser.getLastName());
+                    return true;
+                }).orElse(false);
+    }
+
+
 }
