c323ff924ac8af32d619d34fcba38487e6bafc4d
Berkay Aytekin <158179274+bAytekin5@users.noreply.github.com>
2025-10-31 20:49:46 +0300

    Clean up UserService by removing unused code
    Removed commented-out code related to user management.

--- ecom/src/main/java/com/berkay/ecom/app/service/UserService.java
+++ ecom/src/main/java/com/berkay/ecom/app/service/UserService.java
@@ -18,29 +18,23 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
-//    private List<User> users = new ArrayList<User>();
-//    private Long nextId = 1L;
 
     public List<UserResponse> fetchAllUsers() {
         return userRepository.findAll().stream()
                 .map(userMapper::mapToUserResponse)
                 .collect(Collectors.toList());
-//        return users;
     }
 
     public User addUser(UserRequest request) {
         User user = new User();
         userMapper.updateUserFromRequest(user, request);
         return userRepository.save(user);
-//        user.setId(nextId++);
-//        users.add(user);
     }
 
 
     public Optional<UserResponse> fetchUser(Long id) {
         return userRepository.findById(id)
                 .map(userMapper::mapToUserResponse);
-//        return users.stream().filter(u -> u.getId() == id).findFirst().orElse(null);
     }
 
     public User updateUser(Long id, UserRequest updatedUserRequest) {
@@ -52,13 +46,5 @@ public class UserService {
                     return userRepository.save(existingUser);
                 })
                 .orElseThrow(() -> new RuntimeException("User with id " + id + " not found"));
-
-//        return users.stream().filter(u -> u.getId() == id)
-//                .findFirst()
-//                .map(existingUser -> {
-//                    existingUser.setFirstName(updatedUser.getFirstName());
-//                    existingUser.setLastName(updatedUser.getLastName());
-//                    return true;
-//                }).orElse(false);
     }
 }
